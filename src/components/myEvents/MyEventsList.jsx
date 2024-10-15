70f8accac0d4898bf0c147f180832e2dfffdc5e8
Hasan <hasan.dev04@gmail.com>
2024-10-15 15:31:13 +0600

    not found image added

--- src/components/myEvents/MyEventsList.jsx
+++ src/components/myEvents/MyEventsList.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import MyEventDetails from "./MyEventDetails";
+import notFoundImg from "../../assets/images/undraw_empty_re_opql.svg";
+import { Typography } from "@material-tailwind/react";
 
 export default function MyEventsList({ events }) {
   return (
     <>
-      {events?.map((event) => (
-        <MyEventDetails event={event} />
-      ))}
+      {events?.length == 0 ? (
+        <div className="h-[100vh] flex-col flex items-center justify-center">
+          <img src={notFoundImg} className="w-80" alt="" />
+          <Typography variant="h6">
+            You have not accepted any event yet.
+          </Typography>
+        </div>
+      ) : (
+        events?.map((event) => <MyEventDetails event={event} />)
+      )}
     </>
   );
 }
--- src/components/organizerEvents/EventOrganizer.jsx
+++ src/components/organizerEvents/EventOrganizer.jsx
@@ -1,13 +1,22 @@
+import { Typography } from "@material-tailwind/react";
 import Layout from "../common/Layout";
 import OrganizersEventDetails from "./OrganizersEventDetails";
+import notFoundImg from "../../assets/images/undraw_empty_re_opql.svg";
 
 export default function EventOrganizer({ events }) {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center">
-        {events?.map((e) => (
-          <OrganizersEventDetails event={e} />
-        ))}
+        {events?.length == 0 ? (
+          <div className="h-[100vh] flex-col flex items-center justify-center">
+            <img src={notFoundImg} className="w-80" alt="" />
+            <Typography variant="h6">
+              You have created any event yet.
+            </Typography>
+          </div>
+        ) : (
+          events?.map((e) => <OrganizersEventDetails event={e} />)
+        )}
       </div>
     </Layout>
   );
